6c02c12a8831eaedc7bd9313eae607f74861989c
Dan Robertson <dan@productintuition.com>
2018-12-16 10:21:17 -0800

    Move images directory into public directory and setup static route in app.js to serve files from public directory

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Serve static files from the public folder
+app.use(express.static('public'));
+
+// Set Pug as the view engine for Express
+app.set('view engine', 'pug');
+
+
+// 🤔 From the help doc's Hello World example
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
